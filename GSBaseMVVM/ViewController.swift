234f50d3e92ae0ae52ae2175da83941383cadbed
igatsha <gati1993@gmail.com>
2020-02-25 22:49:13 +0530

    Initial Commit

---
+++ GSBaseMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GSBaseMVVM
+//
+//  Created by Gati Shah on 25/02/20.
+//  Copyright © 2020 iGatiTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
